6f60b3906b0b3f40941babbecfdcb7d25cb06c25
dhentschel06 <hentschel.david@gmail.com>
2017-10-26 12:36:29 +0200

    do not insert empty nodes

--- xmlBuilder.js
+++ xmlBuilder.js
@@ -12,14 +12,17 @@ function buildXmlString(clubs, filename) {
   _.forEach(clubs, (club) => {
     const clubElement = clubsElement.ele('club')
     _.forEach(club, (clubValue, clubKey) => {
-      if (clubKey !== 'players') {
+      if (clubKey !== 'players' && !!clubValue) {
         clubElement.ele(clubKey, clubValue)
-      } else {
+      } else if (clubKey === 'players' && club.players.length !== 0) {
         const playersElement = clubElement.ele('players')
         _.forEach(club.players, (player) => {
           const playerElement = playersElement.ele('player')
           _.forEach(player, (playerValue, playerKey) => {
+            // eslint-disable-next-line
+          if (!!playerValue) {
             playerElement.ele(playerKey, playerValue)
+          }
           })
         })
       }
